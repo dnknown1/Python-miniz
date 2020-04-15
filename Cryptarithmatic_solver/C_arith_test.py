39b1e4f5d6aa3cedc2573696a6e20c670c0d3733
innocentleaf <debalaya.db@gmail.com>
2020-04-15 21:40:24 +0530

    Add files via upload
    Test module

---
+++ Cryptarithmatic_solver/C_arith_test.py
@@ -0,0 +1,23 @@
+import C_arith
+import time
+
+#Driver
+ip =[
+	['b','a','bc'],
+	['odd','odd','even'],
+	['base','ball','games'],
+	['send','more','money']
+]
+	
+for i in ip:
+	print('problem:',i)	
+	a = time.time()
+	v = C_arith.solve(i)
+	if v:
+		print(f'{len(v)} solution(s) possible')
+		for s in v:
+			print(s)
+	else:
+		print('No solution possible')
+	print('time taken (sec)',round(time.time()-a, 2))
+	print('')
