4d889ee26da73b71218b9558f32cdb11a7d4c8f4
innocentleaf <debalaya.db@gmail.com>
2020-04-15 22:00:46 +0530

    Update C_arith_test.py

--- Cryptarithmatic_solver/C_arith_test.py
+++ Cryptarithmatic_solver/C_arith_test.py
@@ -3,14 +3,14 @@ import time
 
 #Driver
 ip =[
-	['b','a','bc'],
-	['odd','odd','even'],
-	['base','ball','games'],
-	['send','more','money']
+	['b', 'a', 'bc'],
+	['odd', 'odd', 'even'],
+	['base', 'ball', 'games'],
+	['send', 'more', 'money']
 ]
 	
 for i in ip:
-	print('problem:',i)	
+	print('problem:', i)	
 	a = time.time()
 	v = C_arith.solve(i)
 	if v:
@@ -19,5 +19,5 @@ for i in ip:
 			print(s)
 	else:
 		print('No solution possible')
-	print('time taken (sec)',round(time.time()-a, 2))
+	print('time taken (sec)', round(time.time()-a, 2))
 	print('')
